0a82615389fc5581143780014f9102a28a3867f5
OGY <ogy1004@naver.com>
2022-05-06 15:31:51 +0900

    제발 배포 되어주세요

--- backend/src/main/java/grape/demo/controller/HelloController.java
+++ backend/src/main/java/grape/demo/controller/HelloController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 public class HelloController {
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "https://ogyworld.github.io/KNU-DBP-Grape/")
     @GetMapping("hello")
     public List<String> Hello(){
         return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
